3d5099fdd834717f3272ff698da93e4d50c1f0fb
Tim Nieradzik <tim@metastack.pl>
2016-07-21 15:28:11 +0200

    Start implementing training

--- glove.py
+++ glove.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 import os
 import numpy as np
-import data_reader
-np.random.seed(1337)
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
@@ -10,67 +8,78 @@ from keras.utils.np_utils import to_categorical
 from keras.models import Sequential, Model
 from keras.layers import Dense, Input, Flatten, Activation, Merge
 
-HiddenSize1    = 50   # Hidden layer for sequence elements
-HiddenSize2    = 10   # Hidden layer after sequence elements have been merged
-LabelsLength   = 2
-LearningRate   = 0.2
+import data_reader
+
+np.random.seed(1337)
+
+HiddenSize     = 10
 EmbeddingDim   = 300
 MaxWords       = 20000
 SequenceLength = 10
+Labels         = 2
 
-dataset = data_reader.dataset()
-texts = list(map(lambda x: x[1], dataset))
+def prepare(data, labels):
+	VALIDATION_SPLIT = 0.2
+	nb_validation_samples = int(VALIDATION_SPLIT * len(data))
 
-labels = [0, 1]
+	x_train = data[:-nb_validation_samples]
+	y_train = labels[:-nb_validation_samples]
+	x_val   = data[-nb_validation_samples:]
+	y_val   = labels[-nb_validation_samples:]
 
-tokenizer = Tokenizer(nb_words=MaxWords)
-tokenizer.fit_on_texts(texts)
-sequences = tokenizer.texts_to_sequences(texts)
+	return x_train, y_train, x_val, y_val
 
-print(sequences)
-
-# Map each word to its unique index
-# {'you': 1, 'how': 2, 'hello': 3, 'are': 4, 'world': 5}
-word_index = tokenizer.word_index
-print('Found %s unique tokens.' % len(word_index))
-
-DictionarySize = len(word_index) + 1  # V
-
-# Zero-pad every string
-data = pad_sequences(sequences, maxlen=SequenceLength)
-
-print(data)
-
-# TODO Why matrix?
-labels = to_categorical(np.asarray(labels))
-print('Shape of data tensor:', data.shape)
-print('Shape of label tensor:', labels.shape)
-
-model = Sequential()
-model.add(Dense(EmbeddingDim, input_dim=SequenceLength * DictionarySize))
-model.add(Dense(LabelsLength, activation='softmax'))
-model.compile(optimizer='rmsprop',
-  loss='categorical_crossentropy',
-  metrics=['accuracy'])
-
-#model.fit(x_train, y_train, validation_data=(x_val, y_val),
-#          nb_epoch=2, batch_size=128)
-
-# Derive n-grams
-def nGram(n, s):
-    return []
-
-def oneHot(wordIndex):
-	vect = np.zeros(len(word_index) + 1)
-	if wordIndex > 0:
-		vect[wordIndex] = 1
+def oneHot(dictionarySize, wordIndex):
+	vect = np.zeros(dictionarySize)
+	if wordIndex > 0: vect[wordIndex] = 1
 	return vect
 
-def query(sentence):
-	_sequences = tokenizer.texts_to_sequences([sentence])
-	_padded = pad_sequences(_sequences, maxlen=SequenceLength)[0]
-	iptOneHot = [oneHot(i) for i in _padded]
-	concat = np.concatenate(iptOneHot)[np.newaxis]
+def sentenceVector(tokeniser, dictionarySize, sentence):
+	sequences = tokeniser.texts_to_sequences([sentence])
+	# Zero-pad every string
+	padded    = pad_sequences(sequences, maxlen=SequenceLength)[0]
+	iptOneHot = [oneHot(dictionarySize, i) for i in padded]
+	concat    = np.concatenate(iptOneHot)[np.newaxis]
+	return concat
+
+def train(x, y):
+	tokeniser = Tokenizer(nb_words=MaxWords)
+	tokeniser.fit_on_texts(x)
+
+	# Map each word to its unique index
+	word_index = tokeniser.word_index
+
+	dictionarySize = len(word_index) + 1
+
+	data = list(map(lambda sentence: sentenceVector(tokeniser, dictionarySize, sentence), x))
+
+	# sequences = tokeniser.texts_to_sequences(x)
+	# data = pad_sequences(sequences, maxlen=SequenceLength)
+
+	labels = [row[np.newaxis] for row in to_categorical(np.asarray(y))]
+
+#	print('Shape of data tensor:', x.shape)
+#	print('Shape of label tensor:', labels.shape)
+#	print('Dictionary size: ', dictionarySize)
+
+	model = Sequential()
+	model.add(Dense(EmbeddingDim, input_dim=SequenceLength * dictionarySize))
+	model.add(Dense(Labels, activation='softmax'))
+	model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+	x_train, y_train, x_val, y_val = prepare(data, labels)
+
+	model.fit(x_train, y_train, validation_data=(x_val, y_val), nb_epoch=2, batch_size=128)
+
+	return model, tokeniser, dictionarySize
+
+def query(model, tokeniser, dictionarySize, sentence):
 	return model.predict(concat)
 
-print(query("It is"))
+dataset = data_reader.dataset()
+x = list(map(lambda x: x[1], dataset))[0:100]
+y = list(map(lambda x: x[0], dataset))[0:100]
+model, tokeniser, dictionarySize = train(x, y)
+
+print(query(model, tokeniser, dictionarySize, "It is bad"))
+print(query(model, tokeniser, dictionarySize, "It is good"))
