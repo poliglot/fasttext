eed387c84134ec5c453b9ae257ddd059620e7fec
Ihor Kroosh <ihor.kroosh@gmail.com>
2016-07-23 12:17:20 +0300

    fixed mistake with )

--- train.py
+++ train.py
@@ -13,4 +13,4 @@ open('model.json', 'w').write(jsonModel)
 open('model-dictionary-size.dat', 'w').write(str(dictionarySize))
 six.moves.cPickle.dump(tokeniser, open("tokeniser.pkl", "wb"))
 
-model.save_weights('model-' + str(time.time()) + '.h5'
+model.save_weights('model-' + str(time.time()) + '.h5')
